b4db959108fb56790b53bd6d8e12fa23ef6d010d
2531251963 <2531251963@qq.com>
2020-04-04 16:33:00 +0800

    Initial commit

---
+++ src/test/java/org/easyarch/rocketmq/RocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package org.easyarch.rocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
